3d6e213081ad485cea33b11144c9a441a9c19c7f
jn-mic <jackotienokey@gmail.com>
2021-07-18 16:53:36 +0300

    creating a string to return a username on profile model

--- MyPro/models.py
+++ MyPro/models.py
@@ -14,3 +14,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     photo = CloudinaryField('profile_pics/', blank=True)
+
+    def __str__(self):
+        return self.user.username
