5397b07fa4f636186f59828fbcf824a1fe22d7b9
jn-mic <jackotienokey@gmail.com>
2021-07-18 17:48:04 +0300

    generating a class method on project for collecting all projects

--- MyPro/models.py
+++ MyPro/models.py
@@ -47,6 +47,11 @@ class Projects(models.Model):
 
     def save_project(self):
         self.save()
-        
+
     def delete_project(self):
         self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
