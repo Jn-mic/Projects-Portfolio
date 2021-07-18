91b45b87415e9987fa821aea7abf44ffd885da8f
jn-mic <jackotienokey@gmail.com>
2021-07-18 16:51:29 +0300

    adding class profile on model

--- MyPro/models.py
+++ MyPro/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django_countries.fields import CountryField
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = CloudinaryField('profile_pics/', blank=True)
